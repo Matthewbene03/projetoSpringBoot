ed0a39170c2006e34b40d2aa10b7021392fb2495
Matheus Benevenuto <matthewbene03@gmail.com>
2024-06-10 17:58:13 -0300

    H2 database, test profile, JPA

--- src/main/java/com/matthews/projetoCourse/entidades/User.java
+++ src/main/java/com/matthews/projetoCourse/entidades/User.java
@@ -3,9 +3,19 @@ package com.matthews.projetoCourse.entidades;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long idUser;
 	private String nomeUser;
 	private String emailUser;
