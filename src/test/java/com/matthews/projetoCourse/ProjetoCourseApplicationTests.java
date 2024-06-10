301d7c2eb9a94a87dad4b7ce756f227257d41953
Matheus Benevenuto <matthewbene03@gmail.com>
2024-06-10 17:00:04 -0300

    Criação do projeto.

---
+++ src/test/java/com/matthews/projetoCourse/ProjetoCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matthews.projetoCourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
